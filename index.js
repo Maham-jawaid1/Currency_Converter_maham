2f949015941b532f9728c32688b2cb5b4fa70175
Maham-jawaid1 <mahamjawaid5678@gmail.com>
2024-04-17 06:13:06 -0700

    adding currency converter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! //usr/bin/env node 
+#! /usr/bin/env node 
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.bgRed("\n\t Welcome to MAHAM _Currency_Converter_\n"));
